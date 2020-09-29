9a9ad06fbaae0dc95f5c9c79662664ee9cdf736e
Denis Kozhevnikov <denis.bugagashkin@yandex.ru>
2020-09-29 21:55:22 +0500

    gh-pages

---
+++ build/precache-manifest.4888f4650027e81dbe4718334d39c057.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd334552edd88eb635e7a68aff18f9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab1130a509b10b822c29",
+    "url": "/static/css/main.201638f3.chunk.css"
+  },
+  {
+    "revision": "a549cb980bf9d2d898d1",
+    "url": "/static/js/2.096056a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.096056a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1130a509b10b822c29",
+    "url": "/static/js/main.d73d6421.chunk.js"
+  },
+  {
+    "revision": "c5e7b408ed1922744b94",
+    "url": "/static/js/runtime-main.e8402bce.js"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/static/media/add-button.b2fce4ac.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/static/media/edit-button.6bfdb354.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/static/media/like-filled.5bbb05f7.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "/static/media/like.a8acc871.svg"
+  },
+  {
+    "revision": "bb8f2c3ee733b3c686b26b2a0e6e9d23",
+    "url": "/static/media/logo.bb8f2c3e.svg"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "/static/media/pencil.34c46850.svg"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "/static/media/trash.d8078343.svg"
+  }
+]);
